47921ddf0fa3422c12aed57de2b801b45d6c9051
nightfallalharbi <khbsalharbi@gmail.com>
2026-05-05 19:46:56 +0300

    sdsdwqdvcqw

--- index.js
+++ index.js
@@ -1,52 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>#qpp</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>H2ckedByAlharbi</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background: url('https://cdn.discordapp.com/attachments/1490327444438384701/1501253712323149974/Video_Project111.mp4?ex=69fb66f8&is=69fa1578&hm=e898d48b7d013f9884911f7ed3f145c48038de9e280c4cebe807877f18ce7359&') no-repeat center center fixed;
+            background-size: cover;
+            font-family: 'Courier New', Courier, monospace;
+            color: #0000ff;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+            flex-direction: column;
+            text-align: center;
+        }
+        .info {
+            margin-top: 20px;
+            font-size: 1.5em;
+            color: #ff0000; 
 
-<style>
-body {
-    margin: 0;
-    padding: 0;
-    background: url('https://media.discordapp.net/attachments/1501257123416506592/1501257738158604349/FFCAEFC8-30FD-478A-877F-85C7A567641A.png?ex=69fb6ab8&is=69fa1938&hm=6458bc377b0d4a432ad96f674dcbaada299307a2abe1e47bea2ce01222881b93&=&format=webp&quality=lossless') no-repeat center center fixed;
-    background-size: cover;
-    font-family: 'Courier New', Courier, monospace;
-    color: #ffffff;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height: 100vh;
-    flex-direction: column;
-    text-align: center;
-}
-
-.text {
-    margin-top: 20px;
-    font-size: 3em;
-    color: #ffffff;
-    text-shadow: 0px 0px 8px #ff0000, 0px 0px 15px #ffffff, 0px 0px 25px #ffffff;
-    letter-spacing: 4.5px;
-}
-
-iframe {
-    width: 0;
-    height: 0;
-    border: 0;
-}
-</style>
+            letter-spacing: 4.5px; 
+            white-space: pre-wrap;
+        }
+        .text {
+            margin-top: 20px;
+            font-size: 3em; 
+            color: #ff0000; 
+            text-shadow: 0px 0px 8px #ff0000, 0px 0px 15px #0000ff, 0px 0px 25px #ff0000; 
+            letter-spacing: 4.5px; 
+        }
+    </style>
 </head>
-
 <body>
+    <div class="image-container">
 
-<!-- صوت يوتيوب (مخفي) -->
-<iframe
-src="https://www.youtube.com/embed/5pscngj524s?autoplay=1&loop=1&playlist=5pscngj524s"
-allow="autoplay">
-</iframe>
-
-<div class="text">h2cked by alharbi ~ 1337</div>
-<div class="text">Discord.gg/qpp</div>
-
-</body>
-</html>
+    </div>
+    <audio autoplay loop>
+        <source src="https://cdn.discordapp.com/attachments/1501257123416506592/1501262331273810143/maradi.mp3?ex=69fb6eff&is=69fa1d7f&hm=3f9db94cabec2fd947f03f53e5792c813cf0bdec318f4ffc13082613417fb426&">
+        Your browser does not support the audio element.
+    </audio>
+    <div class="text">Hacked By nightfall</div>
+    <div class="text">Discord.gg/qpp</div>
+    <div class="text"></div>
