35cb7a8c1c99cdfd9d807e570acb7e82996e3e0a
nightfallalharbi <khbsalharbi@gmail.com>
2026-05-05 19:33:10 +0300

    ddc

---
+++ index.js
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>#qpp</title>
+
+<style>
+body {
+    margin: 0;
+    padding: 0;
+    background: url('https://media.discordapp.net/attachments/1501257123416506592/1501257738158604349/FFCAEFC8-30FD-478A-877F-85C7A567641A.png?ex=69fb6ab8&is=69fa1938&hm=6458bc377b0d4a432ad96f674dcbaada299307a2abe1e47bea2ce01222881b93&=&format=webp&quality=lossless') no-repeat center center fixed;
+    background-size: cover;
+    font-family: 'Courier New', Courier, monospace;
+    color: #ffffff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    flex-direction: column;
+    text-align: center;
+}
+
+.text {
+    margin-top: 20px;
+    font-size: 3em;
+    color: #ffffff;
+    text-shadow: 0px 0px 8px #ff0000, 0px 0px 15px #ffffff, 0px 0px 25px #ffffff;
+    letter-spacing: 4.5px;
+}
+
+iframe {
+    width: 0;
+    height: 0;
+    border: 0;
+}
+</style>
+</head>
+
+<body>
+
+<!-- صوت يوتيوب (مخفي) -->
+<iframe
+src="https://www.youtube.com/embed/5pscngj524s?autoplay=1&loop=1&playlist=5pscngj524s"
+allow="autoplay">
+</iframe>
+
+<div class="text">h2cked by alharbi ~ 1337</div>
+<div class="text">Discord.gg/qpp</div>
+
+</body>
+</html>
